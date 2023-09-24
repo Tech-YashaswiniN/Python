1720c8cb542a37a087fa86340603d1b0d31d692a
Tech=YashaswiniN <Yashawininanjappajigithub@gmail.com>
2023-09-24 09:55:14 +0530

    1st PythonCode

---
+++ 1python.py
@@ -0,0 +1,11 @@
+num = int(input("Enter the number: "))
+n1, n2 = 0, 1
+print("Fibonacci series is: ")
+print(n1)
+print(n2)
+for i in range(2, num):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    print(n3)
+print()
