fc9db128f397ff089b5d500e788bbc55c368246e
Tech-HemanthKumarN <hemanthnanjappaji@gmail.com>
2024-06-18 22:33:25 +0530

    Numberpad

--- NumberPad.py
+++ NumberPad.py
@@ -4,6 +4,16 @@ Number = (
     (7, 8, 9),
     ("*", 0, "#"),
 )
+
+#or 
+
+# Number = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9],
+#     ["*", 0, "#"],
+# ]
+
 for num in Number:
     for i in num:
         print(i, end=" ")
---
+++ OOPS-BankSystem/AccountHandle.py
@@ -0,0 +1,38 @@
+# Create Account class with 2 attributes - balance & account no. Create methods for debit, credit & printing the balance.
+
+class Account:
+
+    def __init__(self, balance, account_no):
+        self.balance = balance
+        self.account_no = account_no
+    
+    #debit method
+
+    def debit(self, amount):
+        self.balance -= amount
+        print("From", self.account_no , "Rs",amount , "was debited. ")
+        print("-------------------------------------------------")
+        print("Total balance is Rs", self.get_balance(),"\n")
+
+    #creadit method
+
+    def credit(self, amount):
+        self.balance += amount
+        print("**************************************************")
+        print("To", self.account_no ,"Rs", amount , "was credited. ","\n")
+        print("-------------------------------------------------")
+        print("Total balance is Rs", self.get_balance(),"\n")
+        print("**************************************************")
+
+    #Total balance method
+
+    def get_balance(self):
+        return self.balance
+
+acc1 = Account(10000, 343278912342)
+acc1.debit(2000)
+acc1.credit(4000)
+
+acc2 = Account(90000, 399978918924 )
+acc2.debit(10000)
+acc2.credit(2000)
